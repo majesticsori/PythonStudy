a720fe8c53b292b36c9366e6d6f2bb8726d55a68
Sol Lee <majesticsori@gmail.com>
2016-02-15 16:30:52 +0900

    Test

--- com/soris/study/ex6.py
+++ com/soris/study/ex6.py
@@ -1,1 +1,26 @@
-__author__ = 'Administrator'
+# _*_ coding: utf-8 _*_
+
+x = "세상에는 %d 종류의 사람이 있어요." % 10
+binary = "'이진수'"
+do_not = "모르는"
+y = "%s를 아는 사람과 %s 사람." % (binary, do_not)
+z = 'How fin is it!'
+
+print x
+print y
+print z
+
+print "'%s'라고 했어요." % x
+print "'%s'이라고도 했죠." % y
+print "So, %r" % z
+
+
+hilarious = False
+joke_evaluation = "웃기는 농담 아니에요?! %r"
+
+print joke_evaluation % hilarious
+
+w = "이 문자열의 왼쪽 ->"
+e = "<- 이 문자열의 오른쪽"
+
+print w + e
